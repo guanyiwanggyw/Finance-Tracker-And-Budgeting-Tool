38057a245fa9a0af973c9005312b73b151e2b4af
Guanyi Wang <guanyiwang.gyw@gmail.com>
2026-09-21 13:06:34 +0100

    refactor: remove old logout button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import LogoutButton from "../components/LogoutButton";
 import Navbar from "../components/NavBar";
 import "../styles/Home.css";
 
@@ -7,7 +6,6 @@ export default function Home() {
     <div>
       <Navbar />
       <p>Budgeting Overview</p>
-      <LogoutButton />
     </div>
   );
 }
