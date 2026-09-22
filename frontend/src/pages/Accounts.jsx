92d7f82b78e4d5172598a1ba73bab7c1b9ec6e11
Guanyi Wang <guanyiwang.gyw@gmail.com>
2026-09-22 22:06:11 +0100

    feat: show account details when account is clicked

--- frontend/src/pages/Accounts.jsx
+++ frontend/src/pages/Accounts.jsx
@@ -5,10 +5,12 @@ import AccountList from "../features/accounts/AccountList";
 import AccountForm from "../features/accounts/AccountForm";
 import NetWorth from "../features/accounts/NetWorth";
 import "../styles/Account.css";
+import AccountDetailsPanel from "../features/accounts/AccountDetails";
 
 export default function Account() {
   const [accounts, setAccounts] = useState([]);
-
+  const [activeAccount, setActiveAccount] = useState(null);
+  const [activeTransaction, setActiveTransaction] = useState(null);
   useEffect(() => {
     getAccounts();
   }, []);
@@ -25,8 +27,18 @@ export default function Account() {
       <Navbar />
       <NetWorth accounts={accounts} />
       <button className="place-holder">FILTER</button>
-      <AccountList accounts={accounts} />
+      <AccountList accounts={accounts} onSelectAccount={setActiveAccount} />
       <AccountForm onAccountCreated={getAccounts} />
+      {activeAccount && (
+        <>
+          <div className="overlay-bg" onClick={() => setActiveAccount(null)} />
+          <AccountDetailsPanel
+            id={activeAccount}
+            onClose={() => setActiveAccount(null)}
+            onSelectTransaction={setActiveTransaction}
+          />
+        </>
+      )}
     </div>
   );
 }
